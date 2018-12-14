4bfeea900014f21755a49690984eafb42c4286be
Nikke <nikolai@vainu.io>
2018-12-14 11:54:32 +0200

    small fix

--- hyperoptimize_model_train.py
+++ hyperoptimize_model_train.py
@@ -13,7 +13,7 @@ import sys
 from src.ReutersDataset import ReutersDataset
 from src.ReutersModel import ReutersModel
 from src.performance_measures import calculate_f1_score, pAtK
-from hyperopt import hp, tpe, fmin, space_eval, Trials
+from hyperopt import hp, tpe, fmin, space_eval, Trials, MongoTrials
 from pprint import pprint
 
 
