401bed764dc3398c5f93638e66df0b0689fd8b63
Nikke <nikolai@vainu.io>
2019-01-05 20:20:02 +0200

    fix

--- evaluate_trained_models.py
+++ evaluate_trained_models.py
@@ -134,7 +134,7 @@ def get_top_model_params():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-e', '--evaluate', type=int)
+    parser.add_argument('-e', '--evaluate', action='store_true')
     parser.add_argument('-p', '--predict', action='store_true')
 
     args = parser.parse_args()
