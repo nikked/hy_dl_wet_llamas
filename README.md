074b1e6eabcd1ca5622bc8cd49c045a99067b252
nikked <nide@cs.helsinki.fi>
2019-11-13 23:45:15 +0100

    updated README and other minor cleaning

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
-# Wet Llamas: Text project
-> News topic classification from Reuters articles
+## News article classification with Deep Learning
 
-* `competition_results.txt`: Our test set output made with our best model
-* `hyperoptimize_CRNN.py`: the train runner for our best model, the CRNN. This file contains the training process including hyperoptimization, the training itself and logging
-* `src/ReutersModel.py`: the Models we tried for this challenge. `class CRNN(nn.Module):` was the one we used for our best models
-* `evaluate_trained_models.py`: A script for evaluating the f1 scores and accuracy of trained models. It also made outputs predictions with the `-p` argument. The `competition_results.txt` was made with this
+This repository consists of the code for a text classification competition organized at the University of Helsinki. The competition was part of a Deep Learning course. Our team won the challenge :)
+
+The task consists of classifying news articles from Reuters to topic codes. It is a multi-label classification problem: each article can correspond to none, one or multiple topics. We solved this problem with a deep neural network architecture we call CRNN, a hybrid model of a convolutional and a recurrent network.
+
+Our award-winning was quite fancy. For tokenization, we used pre-trained GloVe-embeddings. We optimized the model's various hyperparameters with hyperopt. We used a powerful NVIDIA Quadro P4000 GPU for training. As a result, we achieved 97% accuracy for one label prediction.
+
+For more details, please refer to the [our full report of the competition entry.](https://github.com/nikked/hy_dl_wet_llamas/blob/master/reuters_article_classification.pdf)
+
+
+
+### Repository content
+* `hyperoptimize_CRNN.py`: The main CLI of the project that runs the neural network. This file facilitates the whole training process including hyperoptimization, the training itself and logging
+* `src/ReutersModel.py`: This file holds the Models we tried for this challenge. `class CRNN(nn.Module):` was the one we used for our best models
+* `src/topic_codes.txt`: The topic codes we were trying to predict
+* `evaluate_trained_models.py`: A script for evaluating the f1 scores and accuracy of trained models. It also makes outputs predictions with the `-p` argument.
 * `evaluate_topic_reasonability.py`: A script that returns the article text and its predicted label names. Used for evaluating whether the model's predictions seem reasonable to a human reader.
-* `megalog_llama.json`: Train logs from our best performing models
+* `CRNN_training_log.json`: This file shows an excerpt of the output of the gridsearch we did for our CRNN model
+* `competition/`: This directory contains the competition test set and our predictions
 
 ## Requirements
 * Python 3.5+
--- src/ReutersModel.py
+++ src/ReutersModel.py
@@ -2,59 +2,6 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
-
-class ReutersModel(nn.Module):
-    def __init__(self, glove, num_filters, bottleneck_fc_dim, use_batch_norm, dropout_pctg, filter_sizes, stride):
-        super(ReutersModel, self).__init__()
-
-        output_dim = 126
-
-        embedding_dim = len(glove.vectors[0])
-        self.embedding = nn.Embedding.from_pretrained(
-            glove.vectors, freeze=True)
-
-        self.convs = nn.ModuleList(
-            [nn.Conv2d(1, num_filters,
-                       (filter, embedding_dim),
-                       stride=stride)
-             for filter in filter_sizes])
-
-        self.batch_norms = nn.ModuleList(
-            [nn.BatchNorm2d(num_filters) for filter in filter_sizes])
-
-        self.fc1 = nn.Linear(len(filter_sizes) *
-                             num_filters, bottleneck_fc_dim)
-        self.fc2 = nn.Linear(bottleneck_fc_dim, output_dim)
-        self.use_batch_norm = use_batch_norm
-
-        self.dropout = nn.Dropout(dropout_pctg)
-
-    def forward(self, X):
-        X = self.embedding(X)
-
-        conved_layers = []
-        for idx, conv in enumerate(self.convs):
-            X_conv = conv(X.unsqueeze(1))
-            if self.use_batch_norm:
-
-                bn_func = self.batch_norms[idx]
-                X_conv = bn_func(X_conv)
-
-            X_conv = F.relu(X_conv.squeeze(-1))
-            conved_layers.append(X_conv)
-
-        # Pooling
-        pooled = [F.max_pool1d(conv, conv.shape[-1]).squeeze(-1)
-                  for conv in conved_layers]
-
-        X = torch.cat((pooled), dim=-1)
-        X = self.fc1(X)
-        X = F.relu(X)
-        X = self.dropout(X)
-        X = self.fc2(X)
-        return X
-
-
 class CRNN(nn.Module):
     def __init__(self, glove, num_filters, bottleneck_fc_dim, use_batch_norm,
                  dropout_pctg, filter_sizes, stride,
@@ -124,6 +71,58 @@ class CRNN(nn.Module):
         X = self.fc2(X)
         return X
 
+
+
+class ReutersModel(nn.Module):
+    def __init__(self, glove, num_filters, bottleneck_fc_dim, use_batch_norm, dropout_pctg, filter_sizes, stride):
+        super(ReutersModel, self).__init__()
+
+        output_dim = 126
+
+        embedding_dim = len(glove.vectors[0])
+        self.embedding = nn.Embedding.from_pretrained(
+            glove.vectors, freeze=True)
+
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(1, num_filters,
+                       (filter, embedding_dim),
+                       stride=stride)
+             for filter in filter_sizes])
+
+        self.batch_norms = nn.ModuleList(
+            [nn.BatchNorm2d(num_filters) for filter in filter_sizes])
+
+        self.fc1 = nn.Linear(len(filter_sizes) *
+                             num_filters, bottleneck_fc_dim)
+        self.fc2 = nn.Linear(bottleneck_fc_dim, output_dim)
+        self.use_batch_norm = use_batch_norm
+
+        self.dropout = nn.Dropout(dropout_pctg)
+
+    def forward(self, X):
+        X = self.embedding(X)
+
+        conved_layers = []
+        for idx, conv in enumerate(self.convs):
+            X_conv = conv(X.unsqueeze(1))
+            if self.use_batch_norm:
+
+                bn_func = self.batch_norms[idx]
+                X_conv = bn_func(X_conv)
+
+            X_conv = F.relu(X_conv.squeeze(-1))
+            conved_layers.append(X_conv)
+
+        # Pooling
+        pooled = [F.max_pool1d(conv, conv.shape[-1]).squeeze(-1)
+                  for conv in conved_layers]
+
+        X = torch.cat((pooled), dim=-1)
+        X = self.fc1(X)
+        X = F.relu(X)
+        X = self.dropout(X)
+        X = self.fc2(X)
+        return X
 
 class ReutersModelStacked(nn.Module):
     def __init__(self, glove, num_filters, bottleneck_fc_dim, use_batch_norm, dropout_pctg, filter_sizes, stride):
